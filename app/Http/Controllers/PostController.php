661a7c62b52bdcf14702c380b964aa67ab383381
chiakisite <chiakik0522@gmail.com>
2023-09-27 18:27:09 +0900

    編集機能実装

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use App\Http\Requests\PostRequest;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -13,15 +14,22 @@ class PostController extends Controller
        //blade内で使う変数'posts'と設定。'posts'の中身にgetを使い、インスタンス化した$postを代入。
     }
     
-         public function create(Post $post)
+    
+    public function show(Post $post)
+    {
+        return view('posts.posts')->with(['post' => $post]);
+       //blade内で使う変数'posts'と設定。'posts'の中身にgetを使い、インスタンス化した$postを代入。
+    }
+    
+    public function create(Post $post)
     {
         return view('posts.create')->with(['post' => $post]);
        //blade内で使う変数'posts'と設定。'posts'の中身にgetを使い、インスタンス化した$postを代入。
     }
     
-    public function show(Post $post)
+    public function edit(Post $post)
     {
-        return view('posts.posts')->with(['post' => $post]);
+        return view('posts.edit')->with(['post' => $post]);
        //blade内で使う変数'posts'と設定。'posts'の中身にgetを使い、インスタンス化した$postを代入。
     }
     
@@ -30,6 +38,14 @@ class PostController extends Controller
         $input = $request['post'];
         $post->fill($input)->save();
         return redirect('/posts/' . $post->id);
+    }
+    
+    public function update(PostRequest $request, Post $post)
+    {
+        $input_post = $request['post'];
+        $post->fill($input_post)->save();
+        return redirect('/posts/' . $post->id);
+        
     }
     
 }
